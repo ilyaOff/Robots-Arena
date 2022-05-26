bd758848ae7621db31c63e2b59afc1d72e650579
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-05-26 10:49:41 +0300

    Управление камерой
    Добавлена возможность вращения камеры вокруг объекта

---
+++ Robots_Arena/Assets/CameraControll.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControll : MonoBehaviour
+{
+    [SerializeField]
+    private Transform goal;
+
+    public float maxRange = 10f;
+    public float minRange = 1f;
+    [SerializeField, Range(1, 10)]
+    float R = 4;
+
+    [Range(1, 180)]
+    public float rotateSpeed = 2;
+    [Range(1, 20)]
+    public float moveSpeed = 3;
+   
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //поворот вокруг объекта
+        float dAngle = -Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
+        this.transform.RotateAround(goal.position, Vector3.up, dAngle);
+
+        //поддержка расстояния между камерой и объетом
+        float dr = -Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        R = Mathf.Clamp(R + dr, minRange, maxRange);
+        Vector3 dir = this.transform.position - goal.position;
+        this.transform.position = goal.position + dir.normalized * R;
+
+        //Поворот камеры на цель
+        this.transform.LookAt(goal);
+    }
+}
