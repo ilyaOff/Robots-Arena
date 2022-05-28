973a57a0e678d5209d044d14e7a8d5fb7057d3ed
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-05-28 16:35:43 +0300

    Управление камерой
    Настроено управление камерой. Теперь камера не может наклоняться в пределах углов, а так же не может опуститься за пределы определённых значений высоты

--- Robots_Arena/Assets/CameraControll.cs
+++ Robots_Arena/Assets/CameraControll.cs
@@ -9,8 +9,8 @@ public class CameraControll : MonoBehaviour
 
     public float maxRange = 10f;
     public float minRange = 1f;
-    [SerializeField, Range(1, 10)]
-    float R = 4;
+    [Range(1, 10)]
+    private float R = 4;
 
     [Range(1, 30)]
     public float moveSpeed = 3;
@@ -18,15 +18,18 @@ public class CameraControll : MonoBehaviour
     public float rotateSpeed = 60;
     [Range(1, 180)]
     public float verticalSpeed = 30;
+
     [Range(0, 10)]
     public float minY = 0.2f;
     [Range(2, 20)]
     public float maxY = 10f;
+
     [Range(-15, 45)]
     public float minAngle = -5f;
     [Range(-15, 80)]
     public float maxAngle = 60f;
-
+    private float minSin;
+    private float maxSin;
     void Start()
     {
         if (minY > maxY)
@@ -43,6 +46,8 @@ public class CameraControll : MonoBehaviour
             minAngle = maxAngle;
             maxAngle = swap;
         }
+        minSin = Mathf.Sin(minAngle * Mathf.Deg2Rad);
+        maxSin = Mathf.Sin(maxAngle * Mathf.Deg2Rad);
     }
 
     // Update is called once per frame
@@ -52,38 +57,33 @@ public class CameraControll : MonoBehaviour
         float dAngle = -Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
         this.transform.RotateAround(goal.position, Vector3.up, dAngle);
 
-        
-
-
-
         //поддержка расстояния между камерой и объетом
         float dr = -Input.GetAxis("Mouse ScrollWheel") * moveSpeed ;
         R = Mathf.Clamp(R + dr, minRange, maxRange);
         Vector3 dir = this.transform.position - goal.position;
         this.transform.position = goal.position + dir.normalized * R;
 
+        //поворот вверх-вниз
+        dAngle = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
+
+        float x = this.gameObject.transform.position.x;
+        float z = this.gameObject.transform.position.z;
+        float y = this.gameObject.transform.position.y;
+
+        float minY = Mathf.Max(this.minY, goal.position.y + R * minSin);
+        float maxY = Mathf.Min(this.maxY, goal.position.y + R * maxSin);
+
+        if ( (y <= maxY || dAngle < 0 ) && (y >= minY || dAngle > 0))
+        {
+            this.transform.RotateAround(goal.position, this.transform.right, dAngle);
+            y = this.gameObject.transform.position.y;
+
+            y = Mathf.Clamp(y, minY, maxY);
+            this.gameObject.transform.position = new Vector3(x, y, z);
+        }
+
         //Поворот камеры на цель
         this.transform.LookAt(goal);
 
-        //поворот вверх-вниз
-        dAngle = Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
-        float newAngle = this.transform.eulerAngles.x  + dAngle * Mathf.Rad2Deg;
-        if (newAngle < 180 && newAngle > maxAngle) newAngle = maxAngle;
-        //if (newAngle < 360+minAngle) newAngle =360+ minAngle;
-        if( newAngle > 180 && (newAngle-360 < minAngle)) newAngle = minAngle+360;        
-        dAngle = (newAngle - this.transform.eulerAngles.x) * Mathf.Deg2Rad;
-        Debug.Log(newAngle);
-        Debug.Log(dAngle);
-        //if (newAngle > maxAngle * Mathf.Deg2Rad || newAngle < minAngle * Mathf.Deg2Rad) return;
-        this.transform.RotateAround(goal.position, this.transform.right, dAngle);
-
-        this.gameObject.transform.position = new Vector3(
-                                        this.gameObject.transform.position.x,
-                                        Mathf.Clamp(this.gameObject.transform.position.y,
-                                            Mathf.Max(minY, goal.position.y + R * Mathf.Sin(minAngle * Mathf.Deg2Rad)),
-                                            Mathf.Min(maxY, goal.position.y + R * Mathf.Sin(maxAngle * Mathf.Deg2Rad))),
-                                        this.gameObject.transform.position.z);
-
-        
     }
 }
