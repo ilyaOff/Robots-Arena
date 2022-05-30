f8fae1c186a98523fe36f25df1a8001b0bfc5099
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-05-30 19:38:38 +0300

    Конструктор v1
    Готова первая версия конструктора.
    Можно добавлять детали, можно вращать их вокруг точки крепления.
    Поддержку ориентации убрал, так как она не очень помогает на сфере + ломает повороты на цилиндре.
    
    Управление надо вынести за пределы класса.
    
    Можно добавить движение по точкам с фиксированным шагом

--- Robots_Arena/Assets/ConstructorManager.cs
+++ Robots_Arena/Assets/ConstructorManager.cs
@@ -8,11 +8,17 @@ public class ConstructorManager : MonoBehaviour
     public Camera camera = null;
     [Range(1, 15)]
     public float maxDistanceRay = 10f;
-    
-    Parts tmpDetail = null;
 
     public Button button;//Кнопка добавления детали (исправить)
-    Vector3 saveNormalPlane;
+
+    [Range(0, 90)]
+    public float speedRotate = 30;    
+
+    Parts tmpDetail = null;
+    Vector3 oldPointPlaceDetail = Vector3.zero;//Problems??
+    //Добавить модифицированный режим, в котором будет использоваться
+    //[Range(0.005f, 0.4f)]
+    //public float changePositionDistance = 0.1f;
     public void StartPlacingBilding(Parts prefab)
     {
         if (tmpDetail != null)
@@ -21,14 +27,17 @@ public class ConstructorManager : MonoBehaviour
         tmpDetail = Instantiate(prefab);
         tmpDetail.SetTransparent();
         tmpDetail.gameObject.GetComponent<Collider>().enabled = false;
-        saveNormalPlane = tmpDetail.transform.up;
     }
     void Update()
-    {       
+    {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             button.onClick.Invoke();
         }
+    }
+
+    private void KeyBoardRotation()
+    {
         if (Input.GetKey(KeyCode.Q))
         {
             DetailRotate(0, -1, 0);
@@ -38,71 +47,112 @@ public class ConstructorManager : MonoBehaviour
             DetailRotate(0, 1, 0);
         }
 
+        if (Input.GetKey(KeyCode.R))
+        {
+            DetailRotate(1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            DetailRotate(-1, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            DetailRotate(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            DetailRotate(0, 0, -1);
+        }
     }
+
     void FixedUpdate()
     {
+        KeyBoardRotation();
         if (tmpDetail is null) return;//не выбрана деталь
 
         Transform connectionPoint = tmpDetail.connectionPoints[0];
+        Vector3 shift = -connectionPoint.position + tmpDetail.transform.position;
 
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);         
-        Vector3 position = ray.origin + ray.direction* maxDistanceRay;
-        tmpDetail.transform.position = position;
-
+        
+       
         if (Physics.Raycast(ray, out RaycastHit hit, maxDistanceRay))
         {
-            position = hit.point;
             Parts parts = hit.transform.GetComponent<Parts>();
             if (parts is null)
-            {                
-                tmpDetail.transform.position = position;
+            {
+                tmpDetail.transform.position = hit.point + shift;
                 return;
             }
 
-            //Рассчёт точки, куда надо поставить деталь
-            //Точка пересечения луча + позиция точки привязки в префабе            
-            position = hit.point + connectionPoint.position - tmpDetail.transform.position;
-            tmpDetail.transform.position = position;
+            ChangePlaceDetail(connectionPoint, hit, shift);
 
-            //рассчёт поворота детали
-            Vector3 right = Vector3.Project(connectionPoint.right, saveNormalPlane);
 
-            tmpDetail.transform.rotation =
-                Quaternion.FromToRotation(connectionPoint.right, right)
-                 * tmpDetail.transform.rotation;
-
-            tmpDetail.transform.rotation =
-                Quaternion.FromToRotation(connectionPoint.up, hit.normal)
-                 * tmpDetail.transform.rotation;
-
-            
             //DetailRotate(0,, 0);
             if (Input.GetMouseButtonDown(0))
             {
                 tmpDetail.SetNormal();
-                tmpDetail.gameObject.GetComponent<Collider>().enabled = true;  
-                
+                tmpDetail.gameObject.GetComponent<Collider>().enabled = true;
+
                 tmpDetail = null;
             }
         }
+        else
+        {
+            Vector3 position = ray.origin + ray.direction * maxDistanceRay;
+            tmpDetail.transform.position = position + shift;
+        }
+    }
+    //Рассчёт точки, куда надо поставить деталь
+    private bool ChangePlaceDetail(Transform connectionPoint, RaycastHit hit, Vector3 shift)
+    {        
+        //Точка пересечения луча + позиция точки привязки в префабе            
+        Vector3 position = hit.point;
+
+        //Оптимизировать при медленной работе
+        //if(Vector3.Distance(oldPointPlaceDetail, position) < changePositionDistance)
+        if (oldPointPlaceDetail.Equals( position) )
+        {
+            return false;
+        }
+        tmpDetail.transform.position = position + shift;
+        oldPointPlaceDetail = position;
+
+        //рассчёт поворота детали
+        tmpDetail.transform.rotation =
+            Quaternion.FromToRotation(connectionPoint.up, hit.normal)
+             * tmpDetail.transform.rotation;
+
+        return true;
     }
 
     void DetailRotate(float xAngle, float yAngle, float zAngle)
     {
         if (tmpDetail is null) return;
 
+        float xRad = xAngle * speedRotate * Mathf.Deg2Rad;
+        float yRad = yAngle * speedRotate * Mathf.Deg2Rad;
+        float zRad = zAngle * speedRotate * Mathf.Deg2Rad;
+
         Transform connectionPoint = tmpDetail.connectionPoints[0];
         Vector3 yAxis = connectionPoint.up;
-        //Vector3 yAxis = tmpDetail.transform.up;
         //test, delete this
         testNormalStart = tmpDetail.transform.position;
         testNormal = yAxis;
+        
+        tmpDetail.transform.rotation = Quaternion.AngleAxis(yRad, yAxis)
+                                        * tmpDetail.transform.rotation;
 
-        //tmpDetail.transform.rotation = Quaternion.identity;
-        tmpDetail.transform.rotation = Quaternion.AngleAxis(yAngle, yAxis)* tmpDetail.transform.rotation;
-        //tmpDetail.transform.Rotate(yAxis, yAngle);
-        saveNormalPlane = tmpDetail.transform.up;
+        Vector3 xAxis = Vector3.up;
+
+        tmpDetail.transform.RotateAround(connectionPoint.position,  xAxis, xRad);
+        Vector3 zAxis = Vector3.Cross(yAxis, xAxis);
+        if (zAxis == Vector3.zero)
+            zAxis = Vector3.forward;
+        tmpDetail.transform.RotateAround(connectionPoint.position, zAxis, zRad);
     }
+
     private Vector3 testNormal = Vector3.up;
     private Vector3 testNormalStart = Vector3.zero;
     private void OnDrawGizmos()
