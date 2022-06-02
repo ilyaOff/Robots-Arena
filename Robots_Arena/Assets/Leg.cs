ce50afc572e34093de0f8cabfa5b742d4125e2b4
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-06-02 19:45:40 +0300

    Нога 1
    Добавлена новая деталь - нога. В дальнейшем будет использоваться для управления роботом

---
+++ Robots_Arena/Assets/Leg.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Leg : MonoBehaviour
+{
+    [SerializeField]
+    private HingeJoint horizontalJoint, verticalJoint, kneeJoint;
+
+    public float HorizontalTargetPosition => horizontalJoint.spring.targetPosition;
+    public float VerticalTargetPosition => verticalJoint.spring.targetPosition;
+    public float KneeTargetPosition => kneeJoint.spring.targetPosition;
+    // Start is called before the first frame update
+
+    float time = 0f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        time += Time.fixedDeltaTime;
+        JointSpring spring = kneeJoint.spring;
+        spring.targetPosition = (1 + Mathf.Sin(time)) * (kneeJoint.limits.max - kneeJoint.limits.min)/2
+                                + kneeJoint.limits.min;
+        kneeJoint.spring = spring;
+    }
+}
