33e142299d737feba574bbf67405715fe4e2b93a
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-06-02 19:44:37 +0300

    Модификации конструктора
    Добавлена возможность взятия уже установленной детали.
    Поправлена ответственность классов. Теперь part сама включает и выключает коллайдеры.
    Так же теперь при установке деталей формируется иерархия внутри модели

--- Robots_Arena/Assets/ConstructorManager.cs
+++ Robots_Arena/Assets/ConstructorManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ConstructorManager : MonoBehaviour
@@ -19,14 +17,15 @@ public class ConstructorManager : MonoBehaviour
     //Добавить модифицированный режим, в котором будет использоваться
     //[Range(0.005f, 0.4f)]
     //public float changePositionDistance = 0.1f;
-    public void StartPlacingBilding(Parts prefab)
+
+   
+    public void StartPlacingPart(Parts prefab)
     {
         if (tmpDetail != null)
-            Destroy(tmpDetail);
+            Destroy(tmpDetail.gameObject);
 
         tmpDetail = Instantiate(prefab);
         tmpDetail.SetTransparent();
-        tmpDetail.gameObject.GetComponent<Collider>().enabled = false;
     }
     void Update()
     {
@@ -68,68 +67,56 @@ public class ConstructorManager : MonoBehaviour
 
     void FixedUpdate()
     {
-        KeyBoardRotation();
-        if (tmpDetail is null) return;//не выбрана деталь
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Parts parts = null;
+        Vector3 position = ray.origin + ray.direction * maxDistanceRay;
+        if (Physics.Raycast(ray, out hit, maxDistanceRay))
+        {
+            parts = hit.transform.GetComponent<Parts>();
+            position = hit.point;
+        }
 
+        if (tmpDetail == null) //не выбрана деталь
+        {
+            if (parts != null && Input.GetMouseButtonDown(0))
+            {
+                tmpDetail = parts;
+                tmpDetail.SetTransparent();
+            }
+            return;
+        }
+
+        KeyBoardRotation();
         Transform connectionPoint = tmpDetail.connectionPoints[0];
         Vector3 shift = -connectionPoint.position + tmpDetail.transform.position;
-
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);         
-        
-       
-        if (Physics.Raycast(ray, out RaycastHit hit, maxDistanceRay))
+        //Точка пересечения луча + позиция точки привязки в префабе
+        tmpDetail.transform.position = position + shift;
+            
+        if (parts != null)
         {
-            Parts parts = hit.transform.GetComponent<Parts>();
-            if (parts is null)
+            //Оптимизировать при медленной работе
+            //if(Vector3.Distance(oldPointPlaceDetail, position) < changePositionDistance)
+            if (!oldPointPlaceDetail.Equals(position))
             {
-                tmpDetail.transform.position = hit.point + shift;
-                return;
+                oldPointPlaceDetail = position;
+                tmpDetail.transform.rotation =
+                    Quaternion.FromToRotation(connectionPoint.up, hit.normal)
+                    * tmpDetail.transform.rotation;
             }
 
-            ChangePlaceDetail(connectionPoint, hit, shift);
-
-
-            //DetailRotate(0,, 0);
             if (Input.GetMouseButtonDown(0))
             {
-                tmpDetail.SetNormal();
-                tmpDetail.gameObject.GetComponent<Collider>().enabled = true;
-
+                tmpDetail.SetNormal(hit.transform);
                 tmpDetail = null;
             }
         }
-        else
-        {
-            Vector3 position = ray.origin + ray.direction * maxDistanceRay;
-            tmpDetail.transform.position = position + shift;
-        }
     }
-    //Рассчёт точки, куда надо поставить деталь
-    private bool ChangePlaceDetail(Transform connectionPoint, RaycastHit hit, Vector3 shift)
-    {        
-        //Точка пересечения луча + позиция точки привязки в префабе            
-        Vector3 position = hit.point;
-
-        //Оптимизировать при медленной работе
-        //if(Vector3.Distance(oldPointPlaceDetail, position) < changePositionDistance)
-        if (oldPointPlaceDetail.Equals( position) )
-        {
-            return false;
-        }
-        tmpDetail.transform.position = position + shift;
-        oldPointPlaceDetail = position;
-
-        //рассчёт поворота детали
-        tmpDetail.transform.rotation =
-            Quaternion.FromToRotation(connectionPoint.up, hit.normal)
-             * tmpDetail.transform.rotation;
-
-        return true;
-    }
+    
 
     void DetailRotate(float xAngle, float yAngle, float zAngle)
     {
-        if (tmpDetail is null) return;
+        if (tmpDetail == null) return;
 
         float xRad = xAngle * speedRotate * Mathf.Deg2Rad;
         float yRad = yAngle * speedRotate * Mathf.Deg2Rad;
@@ -145,10 +132,10 @@ public class ConstructorManager : MonoBehaviour
                                         * tmpDetail.transform.rotation;
 
         Vector3 xAxis = Vector3.up;
-
         tmpDetail.transform.RotateAround(connectionPoint.position,  xAxis, xRad);
+
         Vector3 zAxis = Vector3.Cross(yAxis, xAxis);
-        if (zAxis == Vector3.zero)
+        if (zAxis.Equals(Vector3.zero))
             zAxis = Vector3.forward;
         tmpDetail.transform.RotateAround(connectionPoint.position, zAxis, zRad);
     }
--- Robots_Arena/Assets/Parts.cs
+++ Robots_Arena/Assets/Parts.cs
@@ -4,52 +4,58 @@ using UnityEngine;
 
 public class Parts : MonoBehaviour
 {
+    //[SerializeField] 
     public Transform[] connectionPoints;
 
-    private bool isSelected = false;
-    public bool IsSelected
-    {
-        get 
-        { 
-            return isSelected;
-        }
-        set 
-        {
-            isSelected = value;
-            if(connectionPoints.Length > 0)
-                foreach (Transform point in connectionPoints)
-                {
-                    point.gameObject.SetActive(value);
-                }
-        }
-    }
-
     public Renderer meshRenderer;
-
-    void Start()
+    
+    [SerializeField]
+    private Collider[] colliders;
+    void Awake()
     {
-        
+        colliders = this.gameObject.GetComponentsInChildren<Collider>();
     }
 
     public void SetTransparent()
-    {        
-        IsSelected = true;
+    {
+        if (connectionPoints.Length > 0)
+            foreach (Transform point in connectionPoints)
+            {
+                point.gameObject.SetActive(true);
+            }
+        
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
         //Чтобы передать материал по ссылке, используется свойство gameObject
-       
         meshRenderer.material.color = new Color(meshRenderer.material.color.r,
                                     meshRenderer.material.color.g,
                                     meshRenderer.material.color.b,
                                     meshRenderer.material.color.a / 2);
+
+        this.transform.parent = null;
     }
 
-    public void SetNormal()
+    public void SetNormal(Transform parent)
     {
-        IsSelected = false;
+        if (connectionPoints.Length > 0)
+            foreach (Transform point in connectionPoints)
+            {
+                point.gameObject.SetActive(false);
+            }
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = true;
+        }
         //Чтобы передать материал по ссылке, используется свойство gameObject
         meshRenderer.material.color = new Color(meshRenderer.material.color.r,
                                      meshRenderer.material.color.g,
                                      meshRenderer.material.color.b,
                                      meshRenderer.material.color.a * 2);
+
+        this.transform.parent = parent;
     }
 
 
