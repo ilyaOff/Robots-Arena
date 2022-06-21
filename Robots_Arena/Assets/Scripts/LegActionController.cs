c98d3b2d1b6d54d614cd33cf800f62e9a00c9757
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-06-21 11:29:45 +0300

    Анимированный робот
    Попытка создать анимированного робота.
    В ходе работы выяснилось, что стандартный контроллер суставов сильно выворачивает их по осям, по которым не должно происходить вращение. Из чего сделан вывод попробовать написать свои суставы.
    Возможно, не получится восстановиться до этой точки

---
+++ Robots_Arena/Assets/Scripts/LegActionController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegActionController : LegController
+{
+    [SerializeField] private LegAction moving;
+    [SerializeField] private float[] shift;
+    private float timer = 0;
+    protected override void Moving()
+    {
+        timer += Time.fixedDeltaTime;
+        if (timer > moving.MaxTime)
+            timer = 0;
+
+
+        for (int i = 0; i < legs.Count; i++)
+        {
+            float k = i < legs.Count / 2 ? 0 : moving.MaxTime/2;
+            legs[i].NormalizeVerticalAngle = CalculateAngle(moving.vertical, timer + shift[i]);
+            legs[i].NormalizeHipAngle = CalculateAngle(moving.hip, k+timer + shift[i]);
+            legs[i].NormalizeKneeAngle = CalculateAngle(moving.knee, k +timer + shift[i]);
+        }
+    }
+
+    private float CalculateAngle(AnimationCurve curve, float time)
+    {
+        return curve.Evaluate(time/ moving.MaxTime);
+    }
+}
