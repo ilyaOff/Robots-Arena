b79bac01446ab19f2e48afbc46d275e8286a8604
ilyaOff <52914481+ilyaOff@users.noreply.github.com>
2022-06-21 12:00:18 +0300

    Revert "Жесткие суставы"
    This reverts commit a6e39199ef6f0209ed39a4f66d0ee6b50eaf8521.

--- Robots_Arena/Assets/Scripts/Bilder/HingeJointController.cs
+++ Robots_Arena/Assets/Scripts/Bilder/HingeJointController.cs
@@ -1,64 +1,63 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class HingeJointController : MonoBehaviour
+public class HingeJointController
 {
-    [SerializeField] private Transform _connect;
-    [SerializeField] private Vector3 _axis = Vector3.forward;
-    [SerializeField] private Vector3 _correctAxis = Vector3.zero;
+    private HingeJoint joint;
 
-    [SerializeField] private float _minAngle = 0f;
-    [SerializeField] private float _maxAngle = 0f;
-    public float MinAngle => _minAngle ;
-    public float MaxAngle => _maxAngle;
-
-    [SerializeField] private float startAngle = 0f;
-    [SerializeField] private float _angle = 0f;
-    public float JointAngle 
-    { 
-        get => _angle;
+    public float MinAngle => joint.limits.min;
+    public float MaxAngle => joint.limits.max;
+    public float JointAngle
+    {
+        get
+        {
+            return joint.spring.targetPosition;
+        }
         set
         {
-            _angle = Mathf.Clamp(value, MinAngle, MaxAngle);
-            transform.Rotate(_connect.position, _angle);
-        }      
+            JointSpring spring = joint.spring;
+            spring.targetPosition = Mathf.Clamp(value, MinAngle, MaxAngle);
+            joint.spring = spring;
+        }
     }
 
     public float NormalizeJointAngle
     {
-        get => (MaxAngle == MinAngle) ? 1 : (JointAngle - MinAngle) / (MaxAngle - MinAngle);        
-        set => JointAngle = value * (MaxAngle - MinAngle) + MinAngle;
+        get
+        { 
+            return (JointAngle - MinAngle) / (MaxAngle - MinAngle);
+        }
+        set
+        {
+            JointAngle = Mathf.Clamp(value, 0, 1) * (MaxAngle - MinAngle) + MinAngle;
+        }
     }
 
     private Vector3 startPosition;
     private Quaternion startRotation;
+    private float startTarget;
 
-    private void OnDrawGizmosSelected()
+    public HingeJointController(HingeJoint joint)
     {
-        Gizmos.color = Color.green;
-        Vector3 start = _correctAxis + _connect.position;
-        Vector3 end = start + _axis*0.4f;
-        Gizmos.DrawLine(start, end);
-    }
-    private void Start()
-    {
+        this.joint = joint;
+
+        startTarget = joint.spring.targetPosition;
+
+        Transform transform = joint.transform;
         startPosition = transform.localPosition;
         startRotation = transform.localRotation;
-
-        if (MinAngle > MaxAngle)
-        {
-            float swap = _minAngle;
-            _minAngle = _maxAngle;
-            _maxAngle = swap;
-            Debug.LogWarning($"{name}: Min and Max angle incorrect, used swap");
-        }
     }
 
     public void Restart()
     {
+        Transform transform = joint.transform;
+
         transform.localPosition = startPosition;
         transform.localRotation = startRotation;
 
-        JointAngle =  startAngle;
+        JointSpring spring = joint.spring;
+        spring.targetPosition =  startTarget;
+        joint.spring = spring;
     }
-
 }
--- Robots_Arena/Assets/Scripts/Bilder/Leg.cs
+++ Robots_Arena/Assets/Scripts/Bilder/Leg.cs
@@ -2,52 +2,104 @@
 
 public class Leg : MonoBehaviour
 {
-    [SerializeField] private Transform connect;
+    [SerializeField]
+    private HingeJoint verticalJoint, hipJoint, kneeJoint;
+
+    [SerializeField]
+    private Rigidbody connection;
+
+    private HingeJointController vertical, hip, knee;
 
     [SerializeField] private Food food;
     public bool InGround => food.InGround;
 
-    [SerializeField]
-    private HingeJointController vertical, hip, knee;
     public float VerticalAngle
     {
-        get => vertical.JointAngle;
-        set => vertical.JointAngle = value;
+        get
+        {
+            return vertical.JointAngle;
+        }
+        set
+        {
+            vertical.JointAngle = value;
+        }
     }
     public float NormalizeVerticalAngle
     {
-        get => vertical.NormalizeJointAngle;
-        set => vertical.NormalizeJointAngle = value;
+        get
+        {
+            return vertical.NormalizeJointAngle;
+        }
+        set
+        {
+            vertical.NormalizeJointAngle = value;
+        }
     }
 
-    public float HiplAngle
+    public float HipAngle
     {
-        get => hip.JointAngle;
-        set => hip.JointAngle = value;
+        get
+        {
+            return hip.JointAngle;
+        }
+        set
+        {
+            hip.JointAngle = value;
+        }
     }
     public float NormalizeHipAngle
     {
-        get => hip.NormalizeJointAngle;
-        set => hip.NormalizeJointAngle = value;
+        get
+        {
+            return hip.NormalizeJointAngle;
+        }
+        set
+        {
+            hip.NormalizeJointAngle = value;
+        }
     }
 
     public float KneeAngle
     {
-        get => knee.JointAngle;
-        set => knee.JointAngle = value;
+        get
+        {
+            return knee.JointAngle;
+        }
+        set
+        {
+            knee.JointAngle = value;
+        }
     }
     public float NormalizeKneeAngle
     {
-        get => knee.NormalizeJointAngle;
-        set => knee.NormalizeJointAngle = value;
+        get
+        {
+            return knee.NormalizeJointAngle;
+        }
+        set
+        {
+            knee.NormalizeJointAngle = value;
+        }
     }
-    
+
+    private void Awake()
+    {
+        vertical = new HingeJointController(verticalJoint);
+        hip = new HingeJointController(hipJoint);
+        knee = new HingeJointController(kneeJoint);
+    }
+
     public void AttachToBody(Rigidbody body)
     {
         if (body is null)
             throw new System.ArgumentNullException("Body must be not null");
 
-        connect.parent = body.transform;
+        verticalJoint.connectedBody = body;
+        if (connection != null)
+        {
+            body.mass += connection.mass;
+            Destroy(connection);
+        }
     }
 
     public void Restart()
